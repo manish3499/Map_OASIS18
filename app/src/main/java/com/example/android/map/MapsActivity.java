35f43e14795a9473097d1522fee696143506f6ae
Manish <manish3499@gmail.com>
2018-10-15 19:02:42 +0530

    Wraps google map API in a fragment

--- app/src/main/java/com/example/android/map/MapsActivity.java
+++ app/src/main/java/com/example/android/map/MapsActivity.java
@@ -14,7 +14,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap;
 
@@ -23,14 +23,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map_fragment);
+//        mapFragment.getMapAsync(this);
     }
 
 
     /**
-     * Manipulates the map once available.
+//     * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
@@ -38,24 +38,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        mMap.clear();
-        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_retro_json));
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            //TODO: Handle location permission
-        }
 
 
-        // Add a marker in Rotunda and move the camera
-        LatLng rotunda = new LatLng(28.3633546, 75.5871163);
-        mMap.addMarker(new MarkerOptions().position(rotunda).title("BITS Rotunda")).showInfoWindow();
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rotunda, 17.0f));
-    }
 }
